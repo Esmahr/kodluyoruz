d5bad71a77b88dea136010f7c1a58e2dbc13aad8
Esmahr <esmaharmanci40@gmail.com>
2022-06-24 21:09:14 +0300

    second commit

---
+++ src/java101/KDVTutarıHesaplama.java
@@ -0,0 +1,21 @@
+package java101;
+
+import java.util.Scanner;
+
+public class KDVTutarıHesaplama {
+    public static void main(String[] args) {
+
+        double tutar;
+        Scanner scan = new Scanner(System.in);
+        System.out.print("Ucret tutarini giriniz: ");
+        tutar = scan.nextDouble();
+
+        double kdvOrani = tutar * 18 / 100;
+        double kdv = tutar + kdvOrani;
+
+        System.out.println("KDV'siz fiyat: " + tutar);
+        System.out.println("KDV orani: " + kdvOrani);
+        System.out.println("KDV'li fiyat: " + kdv);
+
+    }
+}
