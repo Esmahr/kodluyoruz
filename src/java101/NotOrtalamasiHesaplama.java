ba4d7e6baa878d6b1ad4c625f16b88724f626637
Esmahr <esmaharmanci40@gmail.com>
2022-06-24 20:51:11 +0300

    first commit

---
+++ src/java101/NotOrtalamasiHesaplama.java
@@ -0,0 +1,31 @@
+package java101;
+
+import java.util.Scanner;
+
+public class NotOrtalamasiHesaplama {
+    public static void main(String[] args) {
+
+        int mat, fizik, kimya, turkce, tarih, muzik;
+
+        Scanner scan = new Scanner(System.in);
+
+        System.out.print("Matematik notunuzu giriniz: ");
+        mat = scan.nextInt();
+        System.out.print("Fizik notunuzu giriniz: ");
+        fizik = scan.nextInt();
+        System.out.print("Kimya notunuzu giriniz: ");
+        kimya = scan.nextInt();
+        System.out.print("Turkce notunuzu giriniz: ");
+        turkce = scan.nextInt();
+        System.out.print("Tarih notunuzu giriniz: ");
+        tarih = scan.nextInt();
+        System.out.print("Muzik notunuzu giriniz: ");
+        muzik = scan.nextInt();
+
+        double ortalama = (mat + fizik + kimya + turkce + tarih + muzik) / 6;
+        String sonuc = (ortalama >= 60) ? "Tebrikler gectiniz" : "Maalesef kaldınız";
+
+        System.out.print("\nNot ortalamanız: " + ortalama);
+        System.out.print("  -  " + sonuc);
+    }
+}
